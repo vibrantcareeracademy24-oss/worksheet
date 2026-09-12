c5acde74172c628b7bc34640c162b27fcbea9161
vibrantcareeracademy24-oss <vibrantcareeracademy24@gmail.com>
2026-09-12 13:58:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = "https://kathxxseycwncamlciab.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://kathxxseycwncamlciab.supabase.co";
 
 const SUPABASE_ANON_KEY = "kathxxseycwncamlciab";
